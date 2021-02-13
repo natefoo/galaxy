bfd591cfd6ca4fcf097b4323fc30fd3c3a1a8abc
Nate Coraor <nate@bx.psu.edu>
2021-02-13 14:16:53 -0500

    Add server_name tag when logging handler monitor loop timing to statsd

--- lib/galaxy/jobs/handler.py
+++ lib/galaxy/jobs/handler.py
@@ -297,7 +297,8 @@ class JobHandlerQueue(Monitors):
         if self.job_grabber is not None:
             self.job_grabber.grab_unhandled_items()
         self.__handle_waiting_jobs()
-        log.trace(monitor_step_timer.to_str())
+        tags = {'server_name': self.app.config.server_name},
+        log.trace(monitor_step_timer.to_str(tags=tags))
 
     def __handle_waiting_jobs(self):
         """
